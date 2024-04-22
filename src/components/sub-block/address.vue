1c0c117482352f5e154a4b3f6ab2c425c52c30cf
嘉俊 李 <qq1091192337@outlook.com>
2024-04-23 00:09:34 +0800

    修改了页面布局，修复了项目简介不居中的问题

--- src/components/sub-block/address.vue
+++ src/components/sub-block/address.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-
+import { Notification } from '@arco-design/web-vue';
+const handleNone = () => {
+  Notification.info({
+    title: '敬请期待',
+    content: 'iOS版本暂未发布，敬请期待！',
+  });
+};
 </script>
 
 <template>
   <a-typography>
     <a-typography-title>项目地址</a-typography-title>
     <a-space direction="horizontal" size="large">
-      <a-button type="primary" href="http://example.com/android.apk" style="margin-top: 30px">下载安卓安装包</a-button>
-      <a-button type="primary" href="http://example.com/ios.ipa" style="margin-top: 30px">下载iOS安装包</a-button>
+      <a-button type="primary" href="/app/BridgeApp.apk" style="margin-top: 30px">下载安卓安装包</a-button>
+      <a-button type="primary" @click="handleNone"  style="margin-top: 30px">下载iOS安装包</a-button>
       <a-button type="primary" href="#/login" style="margin-top: 30px">访问网页版</a-button>
     </a-space>
 
